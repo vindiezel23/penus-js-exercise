2fb6624c1769238cac3e82e773cecfbec639c254
Kelvin Penus <kelvinpenus@gmail.com>
2018-08-31 00:33:23 +1000

    WNG Tech Exercise

--- Nomad/ClientApp/src/app/app.module.ts
+++ Nomad/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { Mat}
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Nomad/ClientApp/src/app/home/home.component.ts
+++ Nomad/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  items: boolean[] = [false, false, false, false];
+  constructor() { }
+  toggleActivate($index:number): void {
+    const value = !this.items[$index];
+    this.items = _.map(this.items, false);
+    this.items[$index] = value;
+  }
 }
